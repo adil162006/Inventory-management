edf20a4815aef6b7bb15a0948e138b60f79984aa
Mohd Adil <shaikhmdadil191@gmail.com>
2025-10-20 11:02:46 +0530

    added authentication and side bar feature

---
+++ myapp/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Sidebar from '../components/sidebar';
+
+export default function DashboardPage(){
+    return (
+        <div className="min-h-screen bg-gray-50"> 
+           <Sidebar currentPath="/dashboard"/>    
+                    
+        </div>
+    )
+}
---
+++ myapp/prisma/seed.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient()
+
+
+
+async function main(){
+    const demoUserId = "4263d75a-0cef-4ffe-b9b7-22ba2bb28598"
+      // Create sample products
+  await prisma.product.createMany({
+    data: Array.from({ length: 25 }).map((_, i) => ({
+      userId: demoUserId,
+      name: `Product ${i + 1}`,
+      price: (Math.random() * 90 + 10).toFixed(2),
+      quantity: Math.floor(Math.random() * 20),
+      lowStockAt: 5,
+      createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * (i * 5)),
+    })),
+  });
+
+  console.log("Seed data created successfully!");
+  console.log(`Created 25 products for user ID: ${demoUserId}`);
+
+}
+
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
